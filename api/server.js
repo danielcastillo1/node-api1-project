7d4d84e4efd83935e8b3e958268f8e612a25cb55
Daniel Castillo <danielcastillo89@icloud.com>
2023-07-10 02:17:33 -0500

    finished DELETE

--- api/server.js
+++ api/server.js
@@ -5,6 +5,18 @@ const User = require("./users/model")
 const server = express();
 server.use(express.json());
 
+server.delete("/api/users/:id", async (req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist",
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+})
+
 server.post("/api/users", (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
@@ -58,6 +70,7 @@ server.get("/api/users/:id", (req, res) => {
         })
     })
 })
+
 
 server.use("*", (req, res) => {
     res.status(404).json({
